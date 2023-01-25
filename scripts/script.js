3d08e2b99a15278f743586a75c32e5cad5d67a40
jtoufik <112856590+jtoufik@users.noreply.github.com>
2023-01-25 22:30:03 +0100

    .

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,14 @@ addEventListener('click', (event) => {
     const element = document.getElementsByClassName('toggle-box')[0]
     element.classList.toggle('collapsed')
 })
+
+window.onload = function (){
+
+    const menu_btn = document.querySelector('.menu');
+    const mobile_menu = document.querySelector('.mobile-nav');
+
+    menu_btn.addEventListener('click', function (){
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    });
+}
